cf66ad39adcf0d627bb24441125e8405902cf020
AkshayaPriyanka <akshayapriyankac@gmail.com>
2025-07-10 11:44:15 +0530

    Add files via upload
    Day2
    If condition and operators

---
+++ day2/IfCondition_Learn.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class IfCondition_Learn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=5,d=13;
+		System.out.println("Addition :"+ (a+b));
+		if(d%2!=0) {
+			System.out.println("The number is odd");
+		}
+			else
+				System.out.println("The number is even");
+		
+			
+
+	}
+
+}

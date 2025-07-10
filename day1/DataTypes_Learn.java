d11e5e314158ce1cc863130a22ab8992d8e04859
AkshayaPriyanka <akshayapriyankac@gmail.com>
2025-07-10 11:00:00 +0530

    Add files via upload
    Day1
    
    Datatypes
    SimpleJavaProgram

---
+++ day1/DataTypes_Learn.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class DataTypes_Learn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float temperature = 98.5f;
+		String nameOfThePatient = "Akshaya";
+		boolean fever = false;
+		int dob = 1993;
+		char initial = 'C';
+		System.out.println("Name of the patient :"+nameOfThePatient );
+		System.out.println("Dob :"+dob);
+		System.out.println("Body temperature ="+temperature);
+		System.out.println("Does the patient have fever :"+fever);
+		System.out.println("Initial of the patient :"+initial);
+		
+	
+		
+
+	}
+
+}

60691fffbf40d0c3ced5874436d13d72696791f8
AkshayaPriyanka <akshayapriyankac@gmail.com>
2025-07-13 18:38:34 +0530

    Add files via upload

--- Assignments/FibbonacciSeries_Learn.java
+++ Assignments/FibbonacciSeries_Learn.java
@@ -4,6 +4,18 @@ public class FibbonacciSeries_Learn {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int numbersToPrint=8;
+		int first=0,second=1;
+		System.out.println("Fibonnaci series upto " + numbersToPrint + " terms:");
+		for(int i=0;i<numbersToPrint;i++) 
+		{
+			System.out.println(first);
+		
+		    int next=first+second;
+		        first=second;
+		        second=next;
+		}
+		
 
 	}
 
